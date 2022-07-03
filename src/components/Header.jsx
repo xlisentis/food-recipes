7694a24ffb586cbe727ae7811bf2a9ecbe25e0e9
Vsevolod Hella <xlisentis@gmail.com>
2022-07-03 16:13:35 +0300

    Starting files

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <nav className='cyan darken-4'>
+            <div className='nav-wrapper'>
+                <a href='#!' className='brand-logo center'>
+                    Recipes
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
